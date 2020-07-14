168171794a44abab28663a780ef720c456c9f40e
son1473 <57697373+son1473@users.noreply.github.com>
2020-07-15 08:24:39 +0900

    Create basic_function2.js

---
+++ basic_function2.js
@@ -0,0 +1,21 @@
+function expressMultiplication(num1, num2){
+  console.log(String(num1) + ' * ' + String(num2) + ' = ' + String(num1 * num2))
+}
+
+// 테스트 코드
+expressMultiplication(3, 4);
+expressMultiplication(3, 2);
+expressMultiplication(7, 5);
+expressMultiplication(8, 9);
+expressMultiplication(5, 5);
+expressMultiplication(9, 9);
+
+
+/*결과
+3 * 4 = 12
+3 * 2 = 6
+7 * 5 = 35
+8 * 9 = 72
+5 * 5 = 25
+9 * 9 = 81
+*/
