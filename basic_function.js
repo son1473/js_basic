182d2f6e3120d0f69a4c93b3198fe50c1beb7d93
son1473 <57697373+son1473@users.noreply.github.com>
2020-07-15 08:23:56 +0900

    Create basic_function.js

---
+++ basic_function.js
@@ -0,0 +1,20 @@
+function logParticipant(name){
+  console.log(name + '(이)가 대화에 참여했습니다.');
+}
+
+// 테스트 코드
+logParticipant('동욱');
+logParticipant('신의');
+logParticipant('윤수');
+logParticipant('지은');
+logParticipant('동준');
+logParticipant('영훈');
+
+//결과
+/*동욱(이)가 대화에 참여했습니다.
+신의(이)가 대화에 참여했습니다.
+윤수(이)가 대화에 참여했습니다.
+지은(이)가 대화에 참여했습니다.
+동준(이)가 대화에 참여했습니다.
+영훈(이)가 대화에 참여했습니다.
+*/
